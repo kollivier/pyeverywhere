47987aba117c858cf41842f3c1db744a9549c1d1
kollivier <kevino@theolliviers.com>
2015-03-01 11:49:26 -0800

    Make sure we create the dist directory if it doesn't already exist.

--- native/android/build.sh
+++ native/android/build.sh
@@ -12,4 +12,6 @@ cp $PWD/src/org/kosoftworks/pyeverywhere/*.java $DIR/python-for-android/dist/def
 cd $DIR/python-for-android/dist/default
 python build.py --package $1 --name $2 --version $3 --permission INTERNET --permission WRITE_EXTERNAL_STORAGE --dir $4 debug
 
+mkdir -p $START_DIR/dist/android
+echo "cp bin/$2-$3*.apk $START_DIR/dist/android"
 cp bin/$2-$3*.apk $START_DIR/dist/android
