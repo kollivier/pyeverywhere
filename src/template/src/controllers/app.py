64000281bb40c38e5f80bd7b5ea63fcac022fef6
kollivier <kevino@theolliviers.com>
2015-09-02 08:56:34 -0700

    Remove smart quotes.

--- src/template/src/controllers/app.py
+++ src/template/src/controllers/app.py
@@ -54,7 +54,7 @@ class Application(pew.PEWApp):
             "Our experts describe you as an appallingly dull fellow, unimaginative, timid, lacking in initiative, spineless, easily dominated, no sense of humour, tedious company and irrepressibly drab and awful. And whereas in most professions these would be considerable drawbacks, in chartered accountancy they are a positive boon.",
             "It's not pining. It's passed on. This parrot is no more. It has ceased to be. It's expired and gone to meet its maker. This is a late parrot. It's a stiff. Bereft of life, it rests in peace. If you hadn't nailed it to the perch, it would be pushing up the daisies. It's rung down the curtain and joined the choir invisible. THIS IS AN EX-PARROT.",
             "It's just a flesh wound.",
-            "I’m sorry to have kept you waiting, but I’m afraid my walk has become rather sillier recently."
+            "I'm sorry to have kept you waiting, but I'm afraid my walk has become rather sillier recently."
             "Well you can't expect to wield supreme executive power just because some watery tart threw a sword at you.",
             "All right... all right... but apart from better sanitation, the medicine, education, wine, public order, irrigation, roads, a fresh water system, and public health ... what have the Romans ever done for us?",
             "Nudge, nudge, wink, wink. Know what I mean?",
