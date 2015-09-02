1a75f760fea18b7f9cecf3e175f0a1afb9ac90dd
kollivier <kevino@theolliviers.com>
2015-09-02 12:10:33 -0700

    Make the product version match the module version.

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
+import pew
 
 setup(
     name='pyeverywhere',
-    version='1.0.0.1',
+    version=pew.__version__,
     description='Build and deploy cross-platform desktop and mobile apps using Python.',
     author='Kevin Ollivier',
     author_email='kevin@kosoftworks.com',
