1c9dbd6e9276d977307de37832c864c2b44ef515
kollivier <kevino@theolliviers.com>
2016-02-01 09:06:21 -0800

    Don't close on pause by default and add on_stop handling.

--- src/pew/kivy_pew/__init__.py
+++ src/pew/kivy_pew/__init__.py
@@ -1,16 +1,22 @@
+import logging
+
 from android.runnable import run_on_ui_thread
 
 from kivy.app import App
+
 
 @run_on_ui_thread
 def run_on_main_thread(func, *args, **kwargs):
     func(*args, **kwargs)
 
 app = None
+
+
 def get_app():
     return app
 
-class NativePEWApp(App):                                                                          
+
+class NativePEWApp(App):
     def build(self):
         global app
         app = self
@@ -22,8 +28,13 @@ class NativePEWApp(App):
         pass  # I don't think this is supported natively with Kivy
 
     def on_pause(self):
+        logging.info("on_pause called...")
         # Here you can save data if needed
-        return True                                                                        
+        return False
+
+    def on_stop(self):
+        logging.info("on_stop called...")
+        self.shutdown()
 
     def on_resume(self):
         # Here you can check if any data needs replacing (usually nothing)
