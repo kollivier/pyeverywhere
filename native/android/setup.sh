0249b310eda6b4200099b16acfcf29c967e76ed9
kollivier <kevino@theolliviers.com>
2016-05-03 16:01:17 -0700

    Make the Android SDK and build tools something that can be specified on a per-project basis.

--- native/android/setup.sh
+++ native/android/setup.sh
@@ -1,16 +1,15 @@
 #DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ROOT_DIR="$PWD/native/android"
 
+. $ROOT_DIR/setup.sh
+
 export ANT_VERSION="1.9.6"
 
 export ANDROIDSDK="$ROOT_DIR/android-sdk-macosx"
 export ANDROIDNDKVER=r10e
 export ANDROIDNDK="$ROOT_DIR/android-ndk-$ANDROIDNDKVER"
-export ANDROIDAPI=19
-export ANDROIDBUILDTOOLSVER=19.1.0
 
 export ANT_HOME="$ROOT_DIR/apache-ant-$ANT_VERSION"
 echo "Android NDK is $ANDROIDNDK"
-
 
 export PATH=$ANDROIDNDK:$ANDROIDSDK/platform-tools:$ANDROIDSDK/tools:$ANT_HOME/bin:$PATH
