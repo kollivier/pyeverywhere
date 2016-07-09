8ce2569b45f504a6ac065e69a993eecbdb1bde4e
kollivier <kevino@theolliviers.com>
2016-07-09 13:01:13 -0700

    Silence some noisy but irrelevant logs.

--- native/android/run.sh
+++ native/android/run.sh
@@ -10,4 +10,4 @@ echo "PWD is $PWD"
 adb start-server
 adb install -r $1
 adb shell "am start -a android.intent.action.MAIN -n $2/org.kivy.android.PythonActivity"
-adb logcat python:D *:I
+adb logcat python:D BatteryService:S Sensors:S AudioPolicyManagerALSA:S NowDoodleController:S *:E 
