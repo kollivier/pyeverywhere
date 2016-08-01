d6ef960690d6a1a4eabfd52b811bc87a4a286f57
kollivier <kevino@theolliviers.com>
2016-08-01 13:04:50 -0700

    Make orientation a configurable field.

--- native/android/build.sh
+++ native/android/build.sh
@@ -28,6 +28,12 @@ then
 WHITELIST="--whitelist $7"
 fi
 
+ORIENTATION="--orientation sensor"
+if [ ! -z $8 ]
+then
+ORIENTATION="--orientation $8"
+fi
+
 DIST_DIR="$HOME/.local/share/python-for-android/dists"
 if [ "$(uname)" == "Darwin" ]; then
     DIST_DIR="$HOME/.python-for-android/dists"
@@ -36,7 +42,7 @@ fi
 cd src
 
 p4a clean_dists
-p4a apk --private=$4 --requirements=python2,kivy,pyjnius,android,sqlite3 --package=$1 --name=$2 --dist_name="${DIST_NAME}" --version=$3 --orientation sensor --permission=INTERNET --permission=WRITE_EXTERNAL_STORAGE --bootstrap=sdl2 $ICON $WHITELIST --add-source=$SCRIPT_DIR/src/org/kosoftworks/pyeverywhere
+p4a apk --private=$4 --requirements=python2,kivy,pyjnius,android,sqlite3 --package=$1 --name=$2 --dist_name="${DIST_NAME}" --version=$3 --permission=INTERNET --permission=WRITE_EXTERNAL_STORAGE --bootstrap=sdl2 $ICON $WHITELIST $ORIENTATION --add-source=$SCRIPT_DIR/src/org/kosoftworks/pyeverywhere
 
 mkdir -p $START_DIR/dist/android
 if [ ! -d bin ]
