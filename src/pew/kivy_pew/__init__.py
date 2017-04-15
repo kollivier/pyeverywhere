00ddf6da16c37fa70f49000861fe4fc5e0745a00
kollivier <kevino@theolliviers.com>
2017-04-15 13:22:39 -0700

    Handle function return value in run_on_main_thread.

--- src/pew/kivy_pew/__init__.py
+++ src/pew/kivy_pew/__init__.py
@@ -7,7 +7,7 @@ from runnable import run_on_ui_thread
 
 @run_on_ui_thread
 def run_on_main_thread(func, *args, **kwargs):
-    func(*args, **kwargs)
+    return func(*args, **kwargs)
 
 app = None
 
