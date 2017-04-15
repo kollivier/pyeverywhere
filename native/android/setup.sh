cd9657fce8100ec7570907a254726300baa1e390
kollivier <kevino@theolliviers.com>
2017-04-15 13:17:43 -0700

    Update Ant version and print SDK dir as well.

--- native/android/setup.sh
+++ native/android/setup.sh
@@ -5,7 +5,7 @@ ROOT_DIR="$PWD/native/android"
 echo "$ROOT_DIR/setup.sh"
 . $ROOT_DIR/setup.sh
 
-export ANT_VERSION="1.9.8"
+export ANT_VERSION="1.9.9"
 
 PLATFORM='linux'
 NDKPLATFORM='linux'
@@ -20,6 +20,7 @@ export ANDROIDNDKVER=r10e
 export ANDROIDNDK="${ANDROID_ROOT}/android-ndk-$ANDROIDNDKVER"
 
 export ANT_HOME="${ANDROID_ROOT}/apache-ant-$ANT_VERSION"
+echo "Android SDK is $ANDROIDSDK"
 echo "Android NDK is $ANDROIDNDK"
 
 export PATH=$ANDROIDNDK:~/.local/bin:$ANDROIDSDK/platform-tools:$ANDROIDSDK/tools:$ANT_HOME/bin:$PATH
