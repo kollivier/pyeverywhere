9fdba3b260fbf82a3e2e7caadd2be8bed881f7c5
kollivier <kevino@theolliviers.com>
2018-03-24 11:13:43 -0700

    Switch to install_requires to ensure pip install will install the modules.

--- setup.py
+++ setup.py
@@ -14,7 +14,10 @@ setup(
     license='APL',
     package_dir={'': 'src'},
     packages=find_packages('src'),
-    setup_requires=['requests'],
+    install_requires=['requests', 'six', 'virtualenv-api', 'pbxproj'],
+    extras_require={
+        ':platform_machine=="x86_64"': 'wxPython'
+    },
     # test_suite = 'your.module.tests',
     entry_points={
         'console_scripts': [
