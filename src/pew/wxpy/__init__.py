f06671303577d30489667238e81f08efa888ef8e
kollivier <kevino@theolliviers.com>
2018-08-19 13:12:02 -0700

    Add native dialog support for wxPython.

--- src/pew/wxpy/__init__.py
+++ src/pew/wxpy/__init__.py
@@ -9,6 +9,46 @@ def run_on_main_thread(func, *args, **kwargs):
 app = None
 
 
+def choose_file(callback):
+    picked_file = None
+    if app and app.GetTopWindow():
+        def call_file_dialog(callback):
+            dlg = wx.FileDialog(app.GetTopWindow(), "Select a file to open")
+            result = dlg.ShowModal()
+            if result != wx.ID_CANCEL:
+                callback(dlg.GetPath())
+            else:
+                callback(None)
+        run_on_main_thread(call_file_dialog, callback)
+    else:
+        logging.warning("choose_file called without a fully initialized app")
+
+    return picked_file
+
+
+def show_save_file_dialog(options, callback):
+    if app and app.GetTopWindow():
+        def call_file_dialog(options, callback):
+
+            wildcards = []
+            if 'types' in options:
+                types = options['types']
+                for type_name in types:
+                    wildcards.append("{}(*.{})|*.{}".format(type_name, types[type_name], types[type_name]))
+            wildcard = ';'.join(wildcards)
+            logging.debug("wildcard = {}".format(wildcard))
+            dlg = wx.FileDialog(app.GetTopWindow(), "Save file as...", defaultFile="", wildcard = wildcard, style = wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT)
+            result = dlg.ShowModal()
+            if result != wx.ID_CANCEL:
+                callback(dlg.GetPath())
+            else:
+                callback(None)
+        run_on_main_thread(call_file_dialog, options, callback)
+    else:
+        logging.warning("choose_file called without a fully initialized app")
+
+
+
 def get_app():
     return app
 
