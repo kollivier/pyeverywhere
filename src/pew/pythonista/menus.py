417d99b426e5a956e60a7a1dc9a3e32af3a3b561
Kevin Ollivier <kevino@theolliviers.com>
2021-02-02 15:05:17 -0800

    Ensure we import the base shortcut class on platforms that use the stub implementations as well.

--- src/pew/pythonista/menus.py
+++ src/pew/pythonista/menus.py
@@ -1,6 +1,7 @@
 from ..menus import PEWMenuBase
 from ..menus import PEWMenuBarBase
 from ..menus import PEWMenuItemBase
+from ..menus import PEWShortcut
 
 
 # iOS apps don't have menus, so use these stub classes for cross-platform code
