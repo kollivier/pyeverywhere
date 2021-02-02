61e97936fe91c88d5d9d7dbf3f1cad71dca192e8
Kevin Ollivier <kevino@theolliviers.com>
2021-02-02 15:03:52 -0800

    Make sure we uninstall the previous build when running.

--- native/android/run.sh
+++ native/android/run.sh
@@ -6,6 +6,7 @@ cd "$DIR"
 echo "PWD is $PWD"
 
 adb start-server
+adb uninstall $2
 adb install -r $1
 adb shell "am start -a android.intent.action.MAIN -n $2/org.kivy.android.PythonActivity"
 adb logcat PythonActivity:V python:D BatteryService:S Sensors:S SensorService:S WifiAutoJoinController:S WifiTrafficPoller:S WifiAutoJoinController:S WifiStateMachine:S NetworkController:S WifiConfigStore:S AudioPolicyManagerALSA:S NowDoodleController:S chromium:I *:E 
